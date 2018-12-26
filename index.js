d0f2086a125ccabdda34b6965c16b623492e5902
Darkhammer12 <Darkhammer12@protonmail.com>
2018-12-26 15:08:46 -0700

    sync on connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('.pubsub');
 
@@ -7,6 +8,11 @@ const PubSub = require('.pubsub');
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+
+
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -27,7 +33,32 @@ app.post('/api/mine', (req, res) => {
    res.redirect('/api/blocks');
  });
 
-const DEFAULT_PORT = 3000;
+
+//
+ const syncWithRootState = () => {
+   request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+     if (!error && response.statusCode === 200) {
+       const rootChain = JSON.parse(body);
+
+       console.log('replace chain on a sync with', rootChain);
+       blockchain.replaceChain(rootChain);
+     }
+   });
+
+   request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
+     if (!error && response.statusCode === 200) {
+       const rootTransactionPoolMap = JSON.parse(body);
+
+       console.log('replace transaction pool map on a sync with', rootTransactionPoolMap);
+       transactionPool.setMap(rootTransactionPoolMap);
+     }
+   });
+ };
+
+
+
+
+
 let PEER_PORT;
 
 if (process.env.GENERATE_PEER_PORT === 'true') {
